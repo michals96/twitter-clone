cf44b2701e10972ccdcd4c482c59cf2b968aab20
michals96 <michal.stefaniuk9@gmail.com>
2022-06-30 00:02:03 +0200

    Hello world, init project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter 2.0</title>
+      </Head>
+
+      <h1>Hello world!</h1>
+
+    </div>
+  )
+}
+
+export default Home
