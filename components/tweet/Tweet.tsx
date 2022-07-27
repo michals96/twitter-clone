ce67fb62d8d602b273d407ca9c9d7f1433ea17f8
michals96 <michal.stefaniuk9@gmail.com>
2022-07-28 01:15:57 +0200

    Tweet component done

--- components/tweet/Tweet.tsx
+++ components/tweet/Tweet.tsx
@@ -8,9 +8,13 @@ interface Props {
 
 function Tweet({ tweet }: Props) {
   return (
-    <div>
-      <div>
-        <img src={tweet.profileImg} alt=""></img>
+    <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg}
+          alt=""
+        ></img>
         <div>
           <div className="flex items-center space-x-1">
             <p className="mr-1 font-bold">{tweet.username}</p>
@@ -19,6 +23,14 @@ function Tweet({ tweet }: Props) {
             </p>
             <TimeAgo className="text-sm text-gray-500" date={tweet.createdAt} />
           </div>
+          <p className="pt-1">{tweet.text}</p>
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              alt=""
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
+            />
+          )}
         </div>
       </div>
     </div>
