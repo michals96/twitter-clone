fad5788d25587056252956ee8cee59c43e31a6f7
michals96 <michal.stefaniuk9@gmail.com>
2023-01-12 00:48:37 +0100

    Fix imports

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,10 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetServerSideProps } from "next";
 import Head from "next/head";
-import Feed from "../../components/feed/Feed";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Widgets from "../../components/widgets/Widgets";
-import { fetchTweets } from "../../utils/fetchTweets";
 import { Tweet } from "../types/tweet";
+import Feed from "../components/feed/Feed";
+import Widgets from "../components/widgets/Widgets";
+import Sidebar from "../components/sidebar/Sidebar";
+import {fetchTweets} from "../utils/fetchTweets";
 
 interface Props {
   tweets: Tweet[];
@@ -22,7 +22,7 @@ const Home = ({ tweets }: Props) => {
       <main className="grid grid-cols-9 ">
         <Sidebar />
         <Feed tweets={tweets} />
-        <Widgets />
+        <Widgets></Widgets>
       </main>
     </div>
   );
